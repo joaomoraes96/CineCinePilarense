d49938921b1dde8376b9cbc17b2a011a5d552e10
João Moraes <joao11.moraes@hotmail.com>
2024-09-03 18:01:42 -0300

    formulario criado

--- js/script.js
+++ js/script.js
@@ -29,6 +29,76 @@ btnMenuMob.addEventListener('click', ()=>{
 })
 
 
-// efeito botão
+// formulario
 
+class FormSubmit{
+    constructor(configuracoes) {
+        this.configuracoes = configuracoes;
+        this.form = document.querySelector(configuracoes.form);
+        this.formButton = document.querySelector(configuracoes.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
 
+    displaySuccess() {
+        this.form.innerHTML = this.configuracoes.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.configuracoes.error;
+    }
+
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+
+    onSubmission(event) {
+        event.preventDefault();
+        event.target.disabled = true;
+        event.target.innerText = "Enviando...";
+    }
+
+    async sendForm(event) {
+        try {
+            this.onSubmission(event);
+       await fetch(this.url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+
+            },
+            body: JSON.stringify(this.getFormObject()),
+            });
+
+            this.displaySuccess();
+            
+         } catch (error) {
+            this.displayError();
+            throw new Error(error);
+         }
+    }
+
+  
+    init(){
+        if (this.form) this.formButton.addEventListener("click", () => this.sendForm); 
+        return this;
+
+    }
+}
+ 
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='sucess'>Mensagem enviada!</h1>",
+    error: "<h1 class= 'error'>Não foi possível enviar sua mensagem.</h1>",
+
+});
+formSubmit.init();
