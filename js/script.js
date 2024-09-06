ae7827b488a4dff2eb27a1a04cb8782055791865
João Moraes <joao11.moraes@hotmail.com>
2024-09-06 12:03:05 -0300

    rolagem menu

--- js/script.js
+++ js/script.js
@@ -102,3 +102,49 @@ const formSubmit = new FormSubmit({
 
 });
 formSubmit.init();
+
+// menu rolagem
+
+let links = document.querySelectorAll('.js-link');
+let sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let heightSection = section.offsetHeight;
+        let idSection = section.getAttribute('id')
+        
+        // if(top >= offset && top < offset + heightSection) {
+        //     links.forEach(link => {
+        //         link.classList.remove('actived');
+
+        //         document.querySelector(`header a[href*='${idSection}']`).classList.add
+        //         ('actived');
+
+        //     })
+        // }
+
+    // function scrollSection(event) {
+    //     event.preventDefault();
+
+    //     const href = event.currentTarget.getAttribute('href');
+
+    //     const section = document.querySelector(href)
+
+    //     let topSection = section.offsetTop - 112;
+
+    //     window.scrollTop ({
+    //         top: topSection,
+    //         behavior: 'smooth'
+    //     })
+    // }
+
+    // links.forEach(link => {
+    //     link.addEventListener('click, scrollSection')
+    // })
+ 
+
+    })
+})
+
